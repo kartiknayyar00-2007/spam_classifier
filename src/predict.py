248be2ddf98db137e4ab09222f5acc12a5690f20
kartiknayyar00-2007 <kartiknayyar00@gmail.com>
2026-03-31 14:58:52 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,19 @@
+from utils import load_file
 
+model = load_file("../models/model.pkl")
+vectorizer = load_file("../models/vectorizer.pkl")
+
+print("📩 Spam Classifier CLI")
+print("Type 'exit' to quit\n")
+
+while True:
+    text = input("Enter message: ")
+
+    if text.lower() == "exit":
+        print("Exiting...")
+        break
+
+    text_vec = vectorizer.transform([text])
+    result = model.predict(text_vec)
+
+    print("Prediction:", result[0])
