cc761b7942df22f866ca3ecc4713c771b4f3f2db
kartiknayyar00-2007 <kartiknayyar00@gmail.com>
2026-03-31 14:58:04 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,9 @@
+import pickle
 
+def save_file(obj, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(obj, f)
+
+def load_file(filename):
+    with open(filename, "rb") as f:
+        return pickle.load(f)
